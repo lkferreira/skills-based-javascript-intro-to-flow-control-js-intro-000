59f9fcac1680a77be4b17ef169857f4eb162bcae
Lucas Klefasz Ferreira <ferreirak.lucas@gmail.com>
2019-05-03 02:43:58 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -1,6 +1,6 @@
 function basicTeenager(age) {
   if(age >= 13 && age <= 19){
-    return "You are a teenager!"
+    return "You are a teenager!";
   }
 
 }
@@ -8,27 +8,27 @@ function basicTeenager(age) {
 function teenager(age) {
 
   if(age >= 13 && age <= 19){
-    return "You are a teenager!"
+    return "You are a teenager!";
   } else {
-    return "You are not a teenager"
+    return "You are not a teenager";
   }
 
 }
 
 function ageChecker(age) {
   if(age > 13 && age < 19){
-    return "You are a teenager!"
+    return "You are a teenager!";
   } else if(age <= 12){
-    return "You are a kid"
+    return "You are a kid";
   } else{
-    return "You are a grownup"
+    return "You are a grownup";
   }
   }
 
 }
 
 function ternaryTeenager(age) {
-  return age > 13 && age < 19 ? "You are a teenager" : "You are not a teenager"
+  return age > 13 && age < 19 ? "You are a teenager" : "You are not a teenager";
 
 }
 
